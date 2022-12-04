54acda9a3c635f781629171271eabadb48b292ff
mylezuiru <mylesuiru@gmail.com>
2022-12-04 22:30:11 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+
+  // function to render todo items
+  const renderTodoItem = function (e) {
+    e.preventDefault();
+    const todoItemInput = document.querySelector("#new-task-description").value;
+    console.log(todoItemInput);
+    const todoList = document.querySelector("#tasks");
+    const item = document.createElement("li");
+    item.textContent = todoItemInput;
+    // newest to do item to appear first in the list
+    todoList.prepend(item);
+  
+  // deleting task
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = "x";
+  item.append(deleteBtn);
+  const deleteTodo = function () {
+    const li = document.querySelector("li");
+    li.remove();
+  };
+  deleteBtn.addEventListener("click", deleteTodo);
+};
+
+form.addEventListener("submit", renderTodoItem);
 });
+  
